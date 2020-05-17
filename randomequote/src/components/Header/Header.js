0d419cb385714e64d1775bc1ba98b49ebeff7e9e
JEffrey mcintyre <redeyecoding@gmail.com>
2020-05-17 15:58:29 -0400

    Added header componnent and styles for header

---
+++ randomequote/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes  from './Header.module.css';
+
+const header = () => {
+    return (
+        <header className={classes.Header}>
+            Header
+            <div>Logo</div>
+        </header>
+    )
+};
+
+export default header;
